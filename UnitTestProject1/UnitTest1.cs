c2fbe74ac99f429bd2f0d230aa8af180c953c536
jeff994 <hujianbo1981@gmai.com>
2017-12-29 14:39:21 +0800

    ddd

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 cls = new Class1(); 
+
+            Assert.AreEqual(cls.getNum(), 10);
+        }
+    }
+}
